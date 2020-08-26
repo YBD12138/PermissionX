b9458d0164a87485e456c44bbe5cfd8f7d5829c2
YBD <1833896999@qq.com>
2020-08-26 22:57:12 +0800

    成功的开源库用来申请权限

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.permissionx.app
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.permissionx.KingV.PermissionX
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        callpohne.setOnClickListener {
+            PermissionX.request(this,
+                android.Manifest.permission.CALL_PHONE){
+                allGranted , deniedList ->
+                if (allGranted){
+                    call()
+                }else{
+                    Toast.makeText(this,"没有权限",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun call(){
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        }catch (e: SecurityException){
+            e.printStackTrace()
+        }
     }
 }
